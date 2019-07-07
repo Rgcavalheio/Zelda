80927f8e75693e70295ef33d7619bbbcc0593548
Rgcavalheiro <rafael.g.cavalheiro@gmail.com>
2019-07-07 19:10:04 -0300

    agr vai

---
+++ src/com/rafael/world/Camera.java
@@ -0,0 +1,21 @@
+package com.rafael.world;
+
+public class Camera {
+	
+	public static int x;
+	public static int y;
+	
+	public static int clamp(int Atual, int Min, int Max) {
+		if(Atual < Min) {
+			Atual = Min;
+		}
+		if(Atual > Max) {
+			Atual = Max;
+		}
+		return Atual;
+		
+		
+	}
+	
+
+}
---
+++ src/com/rafael/world/World.java
@@ -0,0 +1,115 @@
+package com.rafael.world;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import com.rafael.entities.Bullet;
+import com.rafael.entities.Enemy;
+import com.rafael.entities.Entity;
+import com.rafael.entities.LifePack;
+import com.rafael.entities.Weapon;
+import com.rafael.main.Game;
+
+public class World {
+
+	public static Tile[] tiles;
+	public static int WIDTH, HEIGHT;
+	public static final int TILE_SIZE = 16;
+
+	public World(String path) {
+		try {
+			BufferedImage map = ImageIO.read(getClass().getResource(path));
+			int[] pixels = new int[map.getWidth() * map.getHeight()];
+			WIDTH = map.getHeight();
+			HEIGHT = map.getHeight();
+			tiles = new Tile[map.getWidth() * map.getHeight()];
+
+			map.getRGB(0, 0, map.getWidth(), map.getHeight(), pixels, 0, map.getWidth());
+			for (int i = 0; i < pixels.length; i++) {
+				for (int xx = 0; xx < map.getWidth(); xx++) {
+					for (int yy = 0; yy < map.getHeight(); yy++) {
+
+						int pixelAtual = pixels[xx + (yy * map.getWidth())];
+
+						tiles[xx + (yy * WIDTH)] = new FloorTile(xx * 16, yy * 16, Tile.TILE_FLOOR);
+
+						if (pixelAtual == 0xFFFFFFFF) {
+							// parede
+							tiles[xx + (yy * WIDTH)] = new WallTile(xx * 16, yy * 16, Tile.TILE_WALL);
+
+						} else if (pixelAtual == 0xFF0026FF) {
+							// player
+							Game.player.setX(xx * 16);
+							Game.player.setY(yy * 16);
+						} else if (pixelAtual == 0xFFFF0000) {
+							// enemy
+							Game.entities.add(new Enemy(xx * 16, yy * 16, 16, 16, Entity.ENEMY_EN));
+						} else if (pixelAtual == 0xFFFF7F7F) {
+							// ARMA
+							Game.entities.add(new Weapon(xx * 16, yy * 16, 16, 16, Entity.WEAPON_EN));
+						} else if (pixelAtual == 0xFFFF6A00) {
+							// VIDA
+							Game.entities.add(new LifePack(xx * 16, yy * 16, 16, 16, Entity.LIFEPACK_EN));
+						} else if (pixelAtual == 0xFFFFD800) {
+							// MUNICAO
+							Game.entities.add(new Bullet(xx * 16, yy * 16, 16, 16, Entity.BULLET_EN));
+						}
+
+					}
+
+				}
+
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	
+	public static boolean isFree(int xnext, int ynext) {
+		int x1 = xnext / TILE_SIZE;
+		int y1 = ynext / TILE_SIZE;
+		
+		int x2 = (xnext+TILE_SIZE-1) / TILE_SIZE;
+		int y2 = ynext / TILE_SIZE;
+		
+		int x3 = xnext / TILE_SIZE;
+		int y3 = (ynext +TILE_SIZE-1) / TILE_SIZE;
+		
+		int x4 = (xnext+TILE_SIZE-1) / TILE_SIZE;
+		int y4 = (ynext +TILE_SIZE-1) / TILE_SIZE;
+		
+		return !(
+				(tiles[x1+(y1*World.WIDTH)] instanceof WallTile) || 
+				(tiles[x2+(y2*World.WIDTH)] instanceof WallTile) ||
+				(tiles[x3+(y3*World.WIDTH)] instanceof WallTile) ||
+				(tiles[x4+(y4*World.WIDTH)] instanceof WallTile)
+				);
+		
+		
+		
+	}
+
+	public void render(Graphics g) {
+		int xstart = Camera.x >> 4;
+		int ystart = Camera.y >> 4;
+
+		int xfinal = xstart + (Game.WIDTH >> 4);
+		int yfinal = ystart + (Game.HEIGHT >> 4);
+
+		for (int xx = xstart; xx <= xfinal; xx++) {
+			for (int yy = ystart; yy <= yfinal; yy++) {
+				if (xx < 0 || yy < 0 || xx >= WIDTH || yy >= WIDTH)
+					continue;
+				Tile tile = tiles[xx + (yy * WIDTH)];
+				tile.render(g);
+			}
+
+		}
+	}
+}
