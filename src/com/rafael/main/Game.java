06f9ed03ede0ba89aa701e17c465e28631f2ec0d
Rgcavalheiro <rafael.g.cavalheiro@gmail.com>
2019-07-07 21:32:19 -0300

    erro bizzarro

--- src/com/rafael/main/Game.java
+++ src/com/rafael/main/Game.java
@@ -11,9 +11,11 @@ import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import javax.swing.JFrame;
 
+import com.rafael.entities.Enemy;
 import com.rafael.entities.Entity;
 import com.rafael.entities.Player;
 import com.rafael.graficos.Spritesheet;
@@ -40,15 +42,18 @@ public class Game extends Canvas implements Runnable,KeyListener {
 	private BufferedImage image;
 	
 	public static List<Entity> entities;
-	
-	
+	public static List<Enemy> enemies;
 	public static Spritesheet spritesheet;
 	
 	public static World world;
 	
 	public static Player player;
 	
+	public static Random rand;
+	
 	public Game() {		
+		
+		rand = new Random();
 		
 		
 		addKeyListener(this);
@@ -58,6 +63,7 @@ public class Game extends Canvas implements Runnable,KeyListener {
 		
 		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
 		entities = new ArrayList<Entity>();
+		enemies = new ArrayList<Enemy>();
 		spritesheet = new Spritesheet("/spritesheet.png");
 		player = new Player(0,0,16,16,spritesheet.getSprite(12, 0, 16, 16));
 		entities.add(player);
@@ -151,6 +157,7 @@ public class Game extends Canvas implements Runnable,KeyListener {
 		double delta = 0;
 		int frames = 0;
 		double timer = System.currentTimeMillis();
+		requestFocus();
 
 		while (isRunning) {
 			long now = System.nanoTime();
